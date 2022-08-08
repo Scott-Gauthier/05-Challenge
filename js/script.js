93c46bb80f94231c3ac2ff3f8e158b91eb10d70b
Scott-Gauthier <gauthier.scott@gmail.com>
2022-08-08 14:34:56 -0700

    loop through to create 24 hour text setup for containers next

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 var dateEl = $('#currentDay');
+var timeblockEL = $('#timeblock');
 
 var formattedDate = moment().format("dddd, MMMM Do");
 
 dateEl.text(formattedDate);
+
+for (let i = 0; i <12; i++) {
+    var timeblockItemEL = $(`<li>${i} AM </li>`)
+    timeblockEL.append(timeblockItemEL)
+    console.log(`Test this row ${i} AM`);
+}
+for (let i = 0; i <12; i++) {
+    var timeblockItemEL = $(`<li>${i} PM </li>`)
+    timeblockEL.append(timeblockItemEL)
+    console.log(`Test this row ${i} PM`);
+}
