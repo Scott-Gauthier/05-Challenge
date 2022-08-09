872cab09fe83cf7b6a8ff5d82ad2a4a832a80cc1
Scott-Gauthier <gauthier.scott@gmail.com>
2022-08-09 00:41:32 -0700

    Removed PM code and simplified down to one loop.

--- js/script.js
+++ js/script.js
@@ -3,12 +3,11 @@ var formattedhour = moment().format("H");
 
 var dateEl = $('#currentDay');
 var timeblockEL = $('.container');
-var textEnteredAM = localStorage.getItem("textEnteredAM") || [];
-var textEnteredPM = localStorage.getItem("textEnteredPM") || [];
+var textEntered = localStorage.getItem("textEntered") || [];
 
 dateEl.text(formattedDate + " ");
 
-for (let i = 0; i <12; i++) {
+for (let i = 0; i <24; i++) {
     var timeblockItemRowEL = $(`<div class="row"></div>`);
     if (i == formattedhour) {
         var hourstyle = "present";
@@ -18,36 +17,14 @@ for (let i = 0; i <12; i++) {
         var hourstyle = "past";
     }
     timeblockEL.append(timeblockItemRowEL);
-    timeblockItemRowEL.append(`<div class="col-1 hour text-right pt-3">${i} AM </div>`);
-    var rowContent = JSON.parse(localStorage.getItem("textEnteredAM")) || " ";
+    timeblockItemRowEL.append(`<div class="col-1 hour text-right pt-3">`+moment(i,["HH"]).format("hh A")+`</div>`);
+    var rowContent = JSON.parse(localStorage.getItem("textEntered")) || " ";
     timeblockItemRowEL.append(`<textarea class="col-10 form-control ${hourstyle}" id="text${i}">` + rowContent[i] || "" + `</textarea>`);
     timeblockItemRowEL.append(`<button type="button" class="btn btn-primary col-1 saveBtn" id="btn${i}"><i class="fas fa-save"></i></button>`)
     document.querySelector(`#btn${i}`).addEventListener("click", function(event) {
         event.preventDefault();
-        textEnteredAM[i] = document.querySelector(`#text${i}`).value;
-        localStorage.setItem("textEnteredAM", JSON.stringify(textEnteredAM));
-    console.log(textEnteredAM);
-    });
-}
-
-for (let i = 0; i <12; i++) {
-    var timeblockItemRowEL = $(`<div class="row"></div>`);
-    if (i+12 == formattedhour) {
-        var hourstyle = "present";
-    } else if (i+12 > formattedhour) {
-        var hourstyle = "future";
-    } else {
-        var hourstyle = "past";
-    }
-    timeblockEL.append(timeblockItemRowEL);
-    timeblockItemRowEL.append(`<div class="col-1 hour text-right pt-3">${i} PM </div>`);
-    var rowContent = JSON.parse(localStorage.getItem("textEnteredPM")) || " ";
-    timeblockItemRowEL.append(`<textarea class="col-10 form-control ${hourstyle}" id="text${i+12}">` + rowContent[i] || "" + `</textarea>`);
-    timeblockItemRowEL.append(`<button type="button" class="btn btn-primary col-1 saveBtn" id="btn${i+12}"><i class="fas fa-save"></i></button>`)
-    document.querySelector(`#btn${i+12}`).addEventListener("click", function(event) {
-        event.preventDefault();
-        textEnteredPM[i] = document.querySelector(`#text${i+12}`).value;
-        localStorage.setItem("textEnteredPM", JSON.stringify(textEnteredPM));
-    console.log(textEnteredPM);
+        textEntered[i] = document.querySelector(`#text${i}`).value;
+        localStorage.setItem("textEntered", JSON.stringify(textEntered));
+    console.log(moment(i,["HH"]).format("hh A"));
     });
 }
