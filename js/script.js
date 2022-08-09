bc26bb24db6464fa1f179e0b2e13da4ec1b61a06
Scott-Gauthier <gauthier.scott@gmail.com>
2022-08-08 19:02:28 -0700

    Row textarea changes so that grey=past, current hour=red, future=green

--- js/script.js
+++ js/script.js
@@ -3,27 +3,36 @@ var formattedhour = moment().format("H");
 
 var dateEl = $('#currentDay');
 var timeblockEL = $('.container');
-var textID = $('#'<formattedhour);
 
-dateEl.text(formattedDate + " " + formattedhour);
+dateEl.text(formattedDate + " ");
 
 for (let i = 0; i <12; i++) {
     var timeblockItemRowEL = $(`<div class="row"></div>`);
+    if (i == formattedhour) {
+        var hourstyle = "bg-danger";
+    } else if (i > formattedhour) {
+        var hourstyle = "bg-success";
+    } else {
+        var hourstyle = "bg-secondary";
+    }
     timeblockEL.append(timeblockItemRowEL);
     timeblockItemRowEL.append(`<div class="col-1 bg-light text-right pt-3">${i} AM </div>`);
-    timeblockItemRowEL.append(`<textarea class="col-10 form-control bg-success" id="${i}"></textarea>`);
+    timeblockItemRowEL.append(`<textarea class="col-10 form-control ${hourstyle}"></textarea>`);
     timeblockItemRowEL.append(`<button type="button" class="btn btn-primary col-1 bg-info rounded-right"><i class="fas fa-save"></i></button>`);
-    console.log(`Test this row ${i} AM`);
 }
 for (let i = 0; i <12; i++) {
     var timeblockItemRowEL = $(`<div class="row"></div>`);
+    if (i+12 == formattedhour) {
+        var hourstyle = "bg-danger";
+    } else if (i+12 > formattedhour) {
+        var hourstyle = "bg-success";
+    } else {
+        var hourstyle = "bg-secondary";
+    }
     timeblockEL.append(timeblockItemRowEL);
-    timeblockItemRowEL.append(`<div class="col-1 bg-light text-right pt-3">${i} PM </div>`);
-    timeblockItemRowEL.append(`<textarea class="col-10 form-control bg-success" id="${i+12}"></textarea>`);
+    timeblockItemRowEL.append(`<div class="col-1 bg-light text-right pt-3">${i+12} PM </div>`);
+    timeblockItemRowEL.append(`<textarea class="col-10 form-control ${hourstyle}"></textarea>`);
     timeblockItemRowEL.append(`<button type="button" class="btn btn-primary col-1 bg-info rounded-right"><i class="fas fa-save"></i></button>`);
-    console.log(`Test this row ${i} AM`);
+    console.log(formattedhour);
+    console.log(i+12);
 }
-
-
-//document.getElementById(<formattedhour).addClass('bg-secondary')
-
