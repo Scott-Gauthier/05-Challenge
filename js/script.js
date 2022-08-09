a0af359fea0214ab22c023dd8e5d4b7dd417c311
Scott-Gauthier <gauthier.scott@gmail.com>
2022-08-08 17:51:33 -0700

    Added row container header text area and save button

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
-var dateEl = $('#currentDay');
-var timeblockEL = $('#timeblock');
-
 var formattedDate = moment().format("dddd, MMMM Do");
+var formattedhour = moment().format("H");
 
-dateEl.text(formattedDate);
+var dateEl = $('#currentDay');
+var timeblockEL = $('.container');
+var textID = $('#'<formattedhour);
+
+dateEl.text(formattedDate + " " + formattedhour);
 
 for (let i = 0; i <12; i++) {
-    var timeblockItemEL = $(`<li>${i} AM </li>`)
-    timeblockEL.append(timeblockItemEL)
+    var timeblockItemRowEL = $(`<div class="row"></div>`);
+    timeblockEL.append(timeblockItemRowEL);
+    timeblockItemRowEL.append(`<div class="col-1 bg-light text-right pt-3">${i} AM </div>`);
+    timeblockItemRowEL.append(`<textarea class="col-10 form-control bg-success" id="${i}"></textarea>`);
+    timeblockItemRowEL.append(`<button type="button" class="btn btn-primary col-1 bg-info rounded-right"><i class="fas fa-save"></i></button>`);
     console.log(`Test this row ${i} AM`);
 }
 for (let i = 0; i <12; i++) {
-    var timeblockItemEL = $(`<li>${i} PM </li>`)
-    timeblockEL.append(timeblockItemEL)
-    console.log(`Test this row ${i} PM`);
+    var timeblockItemRowEL = $(`<div class="row"></div>`);
+    timeblockEL.append(timeblockItemRowEL);
+    timeblockItemRowEL.append(`<div class="col-1 bg-light text-right pt-3">${i} PM </div>`);
+    timeblockItemRowEL.append(`<textarea class="col-10 form-control bg-success" id="${i+12}"></textarea>`);
+    timeblockItemRowEL.append(`<button type="button" class="btn btn-primary col-1 bg-info rounded-right"><i class="fas fa-save"></i></button>`);
+    console.log(`Test this row ${i} AM`);
 }
+
+
+//document.getElementById(<formattedhour).addClass('bg-secondary')
+
