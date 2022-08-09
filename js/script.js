25a506d853a046c959227bf7b49c61c6d3d93d68
Scott-Gauthier <gauthier.scott@gmail.com>
2022-08-08 19:31:57 -0700

    Changed from bootstrap formatting to css style.css

--- js/script.js
+++ js/script.js
@@ -9,30 +9,30 @@ dateEl.text(formattedDate + " ");
 for (let i = 0; i <12; i++) {
     var timeblockItemRowEL = $(`<div class="row"></div>`);
     if (i == formattedhour) {
-        var hourstyle = "bg-danger";
+        var hourstyle = "present";
     } else if (i > formattedhour) {
-        var hourstyle = "bg-success";
+        var hourstyle = "future";
     } else {
-        var hourstyle = "bg-secondary";
+        var hourstyle = "past";
     }
     timeblockEL.append(timeblockItemRowEL);
-    timeblockItemRowEL.append(`<div class="col-1 bg-light text-right pt-3">${i} AM </div>`);
+    timeblockItemRowEL.append(`<div class="col-1 hour text-right">${i} AM </div>`);
     timeblockItemRowEL.append(`<textarea class="col-10 form-control ${hourstyle}"></textarea>`);
-    timeblockItemRowEL.append(`<button type="button" class="btn btn-primary col-1 bg-info rounded-right"><i class="fas fa-save"></i></button>`);
+    timeblockItemRowEL.append(`<button type="button" class="btn btn-primary col-1 saveBtn"><i class="fas fa-save"></i></button>`);
 }
 for (let i = 0; i <12; i++) {
     var timeblockItemRowEL = $(`<div class="row"></div>`);
     if (i+12 == formattedhour) {
-        var hourstyle = "bg-danger";
+        var hourstyle = "present";
     } else if (i+12 > formattedhour) {
-        var hourstyle = "bg-success";
+        var hourstyle = "future";
     } else {
-        var hourstyle = "bg-secondary";
+        var hourstyle = "past";
     }
     timeblockEL.append(timeblockItemRowEL);
-    timeblockItemRowEL.append(`<div class="col-1 bg-light text-right pt-3">${i+12} PM </div>`);
+    timeblockItemRowEL.append(`<div class="col-1 hour text-right">${i} PM </div>`);
     timeblockItemRowEL.append(`<textarea class="col-10 form-control ${hourstyle}"></textarea>`);
-    timeblockItemRowEL.append(`<button type="button" class="btn btn-primary col-1 bg-info rounded-right"><i class="fas fa-save"></i></button>`);
+    timeblockItemRowEL.append(`<button type="button" class="btn btn-primary col-1 saveBtn"><i class="fas fa-save"></i></button>`);
     console.log(formattedhour);
     console.log(i+12);
 }
